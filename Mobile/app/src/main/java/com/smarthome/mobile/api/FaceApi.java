abae260eb7bf8e08ed23bcea0a72b317cdc68bf7
soulofhuy <huyhophi123@gmail.com>
2025-02-20 15:54:08 +0700

    add feature to upload image to recognize face

---
+++ Mobile/app/src/main/java/com/smarthome/mobile/api/FaceApi.java
@@ -0,0 +1,16 @@
+package com.smarthome.mobile.api;
+
+import okhttp3.MultipartBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+public interface FaceApi {
+    @Multipart
+    @POST("face_detection/detect/")
+    Call<ResponseBody> uploadImage(
+            @Part MultipartBody.Part image
+    );
+}
