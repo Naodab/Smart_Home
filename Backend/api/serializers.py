8cf347b69196ded74c6e89706ec2d8097c0e94ea
nomenoparty <khronal777@gmail.com>
2025-02-22 21:47:37 +0700

    Test API upload speech, storage file

--- Backend/api/serializers.py
+++ Backend/api/serializers.py
@@ -2,4 +2,4 @@ from rest_framework import serializers
 
 class SpeechSerializer(serializers.Serializer):
   file = serializers.FileField()
-  email = serializers.EmailField()
+  email = serializers.CharField()
