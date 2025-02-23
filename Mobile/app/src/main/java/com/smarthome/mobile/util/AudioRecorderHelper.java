f949b5df8f36122e44eb05f70997294cec83626b
Naodab <nguyenhobadoan110304@gmail.com>
2025-02-23 10:17:26 +0700

    fix audio helper

--- Mobile/app/src/main/java/com/smarthome/mobile/util/AudioRecorderHelper.java
+++ Mobile/app/src/main/java/com/smarthome/mobile/util/AudioRecorderHelper.java
@@ -5,6 +5,7 @@ import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AudioRecorderHelper {
@@ -23,6 +24,7 @@ public class AudioRecorderHelper {
 
     public AudioRecorderHelper(AudioDataListener listener) {
         this.listener = listener;
+        this.recordedData = new ArrayList<>();
     }
 
     @SuppressLint("MissingPermission")
