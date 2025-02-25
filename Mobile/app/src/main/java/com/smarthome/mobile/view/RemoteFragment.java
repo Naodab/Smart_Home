f2868f0809f00c8a66de2674acf7e658a4e663aa
Naodab <nguyenhobadoan110304@gmail.com>
2025-02-25 21:11:43 +0700

    add monitor ui

---
+++ Mobile/app/src/main/java/com/smarthome/mobile/view/RemoteFragment.java
@@ -0,0 +1,28 @@
+package com.smarthome.mobile.view;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.smarthome.mobile.R;
+import com.smarthome.mobile.databinding.FragmentRemoteBinding;
+
+public class RemoteFragment extends Fragment {
+    private FragmentRemoteBinding binding;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentRemoteBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+}
