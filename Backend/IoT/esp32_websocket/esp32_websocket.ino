7d52f85bf504533c4f00bc9e72ee944b76d17c81
nomenoparty <khronal777@gmail.com>
2025-05-07 11:17:16 +0700

    feat(model): speaker recognition

--- Backend/IoT/esp32_websocket/esp32_websocket.ino
+++ Backend/IoT/esp32_websocket/esp32_websocket.ino
@@ -3,16 +3,15 @@
 #include <ArduinoJson.h>
 #include <ESP32Servo.h>
 
-const char* ssid = "...";
-const char* password = "hxh12399";
-const char* serverAddress = "192.168.1.20";  // Thay bằng IP server Django 
+const char* ssid = "Tuyetnu";
+const char* password = "123456789";
+const char* serverAddress = "192.168.1.10";  // Thay bằng IP server Django 
 const int serverPort = 8088;
 
 WebSocketsClient webSocket;
 const int ledPin = 2;
 const int buzzerPin = 15;
 const int servoPin = 5;
-
 Servo myServo;
 
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
